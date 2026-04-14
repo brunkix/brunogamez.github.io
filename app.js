429846f70fb270b54b18e32e7d147c1bf1718a6a
Brunkix <yosoyshogi@gmail.com>
2026-04-14 18:55:45 -0400

    1.1

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Esto sirve para que cargue tus carpetas de assets, css y js correctamente
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor listo en http://localhost:${PORT}`);
+});
